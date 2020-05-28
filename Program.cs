ccd50e52fc1bd8981bfa5b8635eed9b12c848636
VARSHA OBEROI <varsha.r.oberoi@gmail.com>
2020-05-28 12:15:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace gitdemoappconsole
+{
+  class Student
+  {
+    public int StudentID { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Student student = new Student() { StudentID = 1, Name = "Sneha", Address = "Ahmedabad" };
+      Console.WriteLine(student.Name+" "+student.Name+" "+student.Address);
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
